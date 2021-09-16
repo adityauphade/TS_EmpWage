091f85134a213e898702c4d56b1e8a35707958b4
Aditya Uphade <aditya.uphade@morningstar.com>
2021-09-16 13:00:51 +0530

    [Aditya] - Generates Daily Wage

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-//UC1 - 4
+//UC1 - 4 - GENERATE SHIFTS
 var fulltime = 8;
 var parttime = 4;
 var hourlyWage = 20;
-var shift = Math.floor(Math.random() * 10 % 3);
-console.log(shift);
-switch (shift) {
-    case 0:
-        console.log("Full Time Shift");
-        break;
-    case 1:
-        console.log("Part Time Shift");
-        break;
-    case 2:
-        console.log("No Shift");
-        break;
+// function generateShift(){
+//     let shift = Math.floor(Math.random()*10%3)
+//     console.log(shift)
+//     switch(shift){
+//         case 0:
+//             console.log("Full Time Shift")
+//             break
+//         case 1:
+//             console.log("Part Time Shift")
+//             break
+//         case 2:
+//             console.log("No Shift")
+//             break
+//     }
+// }
+// generateShift()
+//UC5 - GENERATE DAILY WAGE
+function generateShift() {
+    var shift = Math.floor(Math.random() * 10 % 3);
+    console.log(shift);
+    switch (shift) {
+        case 0:
+            return fulltime * hourlyWage;
+            // console.log("Full Time Shift")
+            break;
+        case 1:
+            return parttime * hourlyWage;
+            // console.log("Part Time Shift")
+            break;
+        case 2:
+            return 0;
+            // console.log("No Shift")
+            break;
+    }
 }
+console.log("Today's wage: " + generateShift());
--- index.ts
+++ index.ts
@@ -1,18 +1,44 @@
-//UC1 - 4
+//UC1 - 4 - GENERATE SHIFTS
 const fulltime = 8
 const parttime = 4
 const hourlyWage = 20
 
-let shift = Math.floor(Math.random()*10%3)
-console.log(shift)
-switch(shift){
-    case 0:
-        console.log("Full Time Shift")
-        break
-    case 1:
-        console.log("Part Time Shift")
-        break
-    case 2:
-        console.log("No Shift")
-        break
+// function generateShift(){
+//     let shift = Math.floor(Math.random()*10%3)
+//     console.log(shift)
+//     switch(shift){
+//         case 0:
+//             console.log("Full Time Shift")
+//             break
+//         case 1:
+//             console.log("Part Time Shift")
+//             break
+//         case 2:
+//             console.log("No Shift")
+//             break
+//     }
+// }
+
+// generateShift()
+
+//UC5 - GENERATE DAILY WAGE
+function generateShift(){
+    let shift = Math.floor(Math.random()*10%3)
+    console.log(shift)
+    switch(shift){
+        case 0:
+            return fulltime*hourlyWage
+            // console.log("Full Time Shift")
+            break
+        case 1:
+            return parttime*hourlyWage
+            // console.log("Part Time Shift")
+            break
+        case 2:
+            return 0
+            // console.log("No Shift")
+            break
+    }
 }
+
+console.log(`Today's wage: ${generateShift()}`)
