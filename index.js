3f6096f9c9d5643bc3144a4e9f97ef18f5f5acb3
Aditya Uphade <aditya.uphade@morningstar.com>
2021-09-16 12:54:04 +0530

    [Aditya] - Created a function ton Generate shift for an employee

---
+++ index.js
@@ -0,0 +1,17 @@
+//UC1 - 4
+var fulltime = 8;
+var parttime = 4;
+var hourlyWage = 20;
+var shift = Math.floor(Math.random() * 10 % 3);
+console.log(shift);
+switch (shift) {
+    case 0:
+        console.log("Full Time Shift");
+        break;
+    case 1:
+        console.log("Part Time Shift");
+        break;
+    case 2:
+        console.log("No Shift");
+        break;
+}
